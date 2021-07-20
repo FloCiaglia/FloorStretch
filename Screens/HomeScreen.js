95e6d428ce5dd2608a527c2a309a2eb0e77d4aab
FloCiaglia <floriana.ciaglia@gmail.com>
2021-07-19 19:35:54 -0700

    background color

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -3,7 +3,7 @@ import { Text, View, Button, StyleSheet } from "react-native";
 
 const HomeScreen = (props) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.text}>HomeScreen</Text>
       <Text> Lets Check if this updates</Text>
       <Button title='Components' onPress={()=> props.navigation.navigate('Components')}/>
@@ -14,8 +14,16 @@ const HomeScreen = (props) => {
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
+    fontSize: 30,
+    marginLeft: 10
+
+  },
+  container: {
+    backgroundColor: "pink"
   }
+  
 });
+
+
 
 export default HomeScreen;
